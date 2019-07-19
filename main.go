811d81f26faabdb0ce9c3f81b3160b26b3121c8b
Bode Falade <bbfalade@spscommerce.com>
2019-07-19 13:21:23 -0500

    type deck added.

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	cards := []string{"Ace of Diamonds ", newCard()}
+	cards := deck{"Ace of Diamonds ", newCard()}
 	cards = append(cards, "Six of Spades")
 
 	for i, card := range cards {
