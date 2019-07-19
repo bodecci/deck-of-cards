da25da9b09cacc3d9fddd5526dbac635e61a309f
Bode Falade <bbfalade@spscommerce.com>
2019-07-19 10:29:07 -0500

    slice: an array tha can grow or shrink.

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	cards := []string{"Ace of Diamonds ", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds "
 }
