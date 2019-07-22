6eff695dcd51d23d0788af294e9292a2fd03b6bc
Bode Falade <bbfalade@spscommerce.com>
2019-07-22 10:17:02 -0500

    type conversion: deck of cards converted to strings.

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import "fmt"
 
 func main() {
 
+	// cards := newDeck()
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	// type conversion
+	// greeting := "Hi there!"
+	// fmt.Println([]byte(greeting))
+
 	cards := newDeck()
+	fmt.Println(cards.toString())
 
-	cards.print()
-
-	fmt.Println(cards)
 }
