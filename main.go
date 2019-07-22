405e3c98136b11f8f449372bf1038e9879a26c0a
Bode Falade <bbfalade@spscommerce.com>
2019-07-21 21:04:55 -0500

    newDeck function added.

--- main.go
+++ main.go
@@ -4,14 +4,9 @@ import "fmt"
 
 func main() {
 
-	cards := deck{"Ace of Diamonds ", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
 
 	fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Five of Diamonds "
 }
